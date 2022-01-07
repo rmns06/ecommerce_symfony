499fdc43ec3751564eb059e29c17b7e54d76c4e4
Romain <rmns06@gmail.com>
2022-01-07 14:47:15 +0100

    feature/ add role and leave for admin & add voter for category edit

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,11 +6,12 @@ use App\Entity\Category;
 use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class CategoryController extends AbstractController
 {
@@ -45,7 +46,23 @@ class CategoryController extends AbstractController
         $category = $repository->find($id);
         $form = $this->createForm(CategoryType::class, $category);
         $form->handleRequest($request);
+        
+        // si la catégorie n'existe pas
+        if (!$category)
+        {
+            throw $this->createNotFoundException('Cette catégorie n\'existe pas');
+        }
 
+        
+        // Seul le créateur de la catégorie peut la modifier
+        // $user = $this->getUser();
+        // if ($user !== $category->getOwner()) {
+        //     throw new AccessDeniedHttpException("Vous n'êtes pas le créateur de cette catégorie");
+        // }
+        
+        //gestion du droit de modification avec un voter
+
+        $this->denyAccessUnlessGranted('CAN_EDIT', $category, "Vous n'êtes pas le créateur de cette cact");
         if ($form->isSubmitted() && $form->isValid())
         {
             $category->setSlug(strtolower($slugger->slug($category->getName())));
@@ -54,10 +71,6 @@ class CategoryController extends AbstractController
             return $this->redirectToRoute('homepage');
         }
 
-        if (!$category)
-        {
-            throw $this->createNotFoundException('Cette catégorie n\'existe pas');
-        }
         return $this->render('category/edit.html.twig', [
             'form' => $form->createView(),
         ]);
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,6 +20,7 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => "J'accepte les conditions générales d'utilisation.",
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
